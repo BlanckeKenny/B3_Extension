f885d523eb9255d06ea48a5d8789fc9dc7c03dad
wilschok <william.schokkele@howest.be>
2017-02-15 18:56:17 +0100

    string - valid email

--- ConsExtension/Program.cs
+++ ConsExtension/Program.cs
@@ -10,6 +10,9 @@ namespace ConsExtension
     {
         static void Main(string[] args)
         {
+            string email = "extension.method@visualstudio.net";
+            Console.WriteLine($"{email} is emailadres: {email.IsValidEmailAddress()}");
+            Console.ReadKey();
         }
     }
 }
