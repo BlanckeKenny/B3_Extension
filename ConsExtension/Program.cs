8b5c24707e93aae06a7d46e5fcb62e31d214f555
wilschok <william.schokkele@howest.be>
2017-02-16 09:27:40 +0100

    Dynamische lambda's met Func

--- ConsExtension/Program.cs
+++ ConsExtension/Program.cs
@@ -8,10 +8,16 @@ namespace ConsExtension
         static void Main(string[] args)
         {
             string[] animals = { "monkey", "bear", "rabbit", "wolf" };
-            var fourLetterAnimals = animals
-                                        .Where(a => a.Length == 4)
-                                        .OrderByDescending( a => a);
-            foreach(string animal in fourLetterAnimals)
+            Func<string, int> lambda1 = a => a.Length;
+            Func<string, bool> lambda2 = a => a.Length == 4;
+
+            var lengthOfAnimals = animals.Select(lambda1);
+            var fourLetterAnimals = animals.Where(lambda2);
+            foreach (int length in lengthOfAnimals)
+            {
+                Console.WriteLine($"length: {length}");
+            }
+            foreach (string animal in fourLetterAnimals)
             {
                 Console.WriteLine($"{animal} has 4 letters.");
             }
